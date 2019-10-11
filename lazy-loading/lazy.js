5e04bb430c493ef87e2d318e0c6a1fb789f2d4d5
Israel Cruz Ibarra <israel.ibarra@ibm.com>
2019-10-10 20:29:00 -0500

    Lazy Loading - Image Observer

---
+++ lazy-loading/lazy.js
@@ -0,0 +1,30 @@
+const images = [].slice.call(document.querySelectorAll('.dog'));
+
+function lazyLoadPhoto(photo) {
+    const lazyImage = new Image();
+    lazyImage.classList.add('dog');
+    lazyImage.src = photo.dataset.srcSet;
+    lazyImage.onload = () => {
+        const parent = photo.parentNode;
+        parent.replaceChild(lazyImage, photo);
+    }
+}
+
+if('IntersectionObserver' in window) {
+    let imageObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+            if(entry.isIntersecting) {
+                let image = entry.target;
+                const placeHolderSrc = image.dataset.src;
+                image.src = placeHolderSrc;
+                imageObserver.unobserve(image);
+                setTimeout(() => {
+                    lazyLoadPhoto(image);
+                },3000);
+            }
+        });
+    });
+    images.forEach((image) => {
+        imageObserver.observe(image)
+    })
+}
